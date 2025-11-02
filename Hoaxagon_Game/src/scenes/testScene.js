497e3c888b0658d23d22331dcd1e20a74c7aef68
TelmoK <telmo.kiss@gmail.com>
2025-11-02 01:12:51 +0100

    Área de scroll de GameObjects

--- Hoaxagon_Game/src/scenes/testScene.js
+++ Hoaxagon_Game/src/scenes/testScene.js
@@ -1,5 +1,6 @@
 import { IMAGE_KEYS, SCENE_KEYS } from '../utils/CommonKeys.js'
-import { PostBoxObject } from '../systems/post_system/postBoxObject.js'
+//import { PostBoxObject } from '../systems/post_system/postBoxObject.js'
+import { ScrollAreaContainer } from '../systems/scroll_system/scrollAreaContainer.js';
 
 export default class TestScene extends Phaser.Scene {
 	/**
@@ -16,10 +17,11 @@ export default class TestScene extends Phaser.Scene {
 	*/
 	create() {
 
-		const text = "Esto es un texto de sección 0, esto de la 1. ¿Y esto de la 2?\n"
+		/*const text = "Esto es un texto de sección 0, esto de la 1. ¿Y esto de la 2?\n"
 			+ "Esto de la 3, la 4 💡 ¡¡¡La 5 (incluido esto)!!!";
 
-		let post = new PostBoxObject(this, 200, 200, text, 400);
+		let post = new PostBoxObject(this, 200, 200, text, 400);*/
+		let scrollArea = new ScrollAreaContainer(this, 10, 10, 500, 500);
 	}
 
 	update(time, dt) {
---
+++ Hoaxagon_Game/src/systems/scroll_system/scrollAreaContainer.js
@@ -0,0 +1,78 @@
+import { PostBoxObject } from '../post_system/postBoxObject.js';
+
+export class ScrollAreaContainer extends Phaser.GameObjects.Container {
+
+    /**
+     * @type {number}
+     */
+    _width;
+
+    /**
+     * @type {number}
+     */
+    _height;
+
+    /**
+     * @type {Phaser.GameObjects.Container}
+     */
+    _viewportAreaContainer;
+
+    /**
+     * @type {Phaser.GameObjects.Container}
+     */
+    _contentAreaContainer;
+
+    /**
+     * 
+     * @param {Phaser.Scene} scene 
+     * @param {number} positionX 
+     * @param {number} positionY 
+     * @param {number} width 
+     * @param {number} height 
+     */
+    constructor(scene, positionX, positionY, width, height) {
+        super(scene, positionX, positionY);
+
+        this._width = width;
+        this._height = height;
+
+        this._viewportAreaContainer = scene.add.container(positionX, positionY);
+
+        this._contentAreaContainer = scene.add.container(0, -120);
+
+        // The Content Container is inside the Viewport Container and it is the one that moves
+        this._viewportAreaContainer.add(this._contentAreaContainer);
+
+        // temp<<<<<<<<<<<<<<<<<
+        for(let i = 0; i < 10; i++) {
+            this._contentAreaContainer.add(new PostBoxObject(scene, 0, 150*i, "Textito\nDel bueno", 400));
+        }
+        // <<<<<<<<<<<<<<<<<<<<<
+
+        // Definition of the mask that will cut the content out of the bounds of the viewport
+        this.setSize(width, height);
+
+        scene.input.on('wheel', (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
+            const scrollAmount = deltaY * 0.5; // Ajusta la velocidad
+
+            // 1. Calcular la nueva posición Y
+            let newY = this._contentAreaContainer.y - scrollAmount;
+
+            this._contentAreaContainer.y = newY;
+        });
+    }
+
+    /**
+     * 
+     * @param {number} width 
+     * @param {number} height 
+     */
+    setSize(width, height) {
+        const maskShape = this.scene.make.graphics({});
+        maskShape.fillRect(this.x, this.y, width, height);
+
+        const mask = maskShape.createGeometryMask();
+
+        this._viewportAreaContainer.setMask(mask);
+    }
+}
